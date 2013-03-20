c4d495c4174d75d1ef7c684b65f06b80a2c348f7
dannycochran <daniel@cochrans.org>
2013-03-20 13:03:47 -0700

    fixed port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ function start(route, handle) {
     console.log("Request for " + pathname + " received.");
     route(handle, pathname, response, request);
   }
-  http.createServer(onRequest).listen(42953);
+  http.createServer(onRequest).listen(33087);
   console.log("Server has started.");
 }
 
