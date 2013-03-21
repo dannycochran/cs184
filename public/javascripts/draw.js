57c7b96c38367508a08eb5826b757e47680e0209
dannycochran <daniel@cochrans.org>
2013-03-20 23:47:44 -0700

    replaced draw

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -1,31 +1,92 @@
-			globals = {}
-			globals.noteColor = "white";
-			globals.playChecker = 0;
-			paper.view.viewSize = [1440,1000];
-			
-			var count = 150;
-			var path = new Path.Circle(new Point(0, 0), 10);
-			
-			path.style = {
-				fillColor: 'white',
-				strokeColor: 'black'
-			};
-			var symbol = new Symbol(path);
-			
-			for (var i = 0; i < count; i++) {
-				var center = Point.random() * view.size;
-				var placedSymbol = symbol.place(center);
-				placedSymbol.scale(i / count);
-			}
-			function onFrame(event) {
-				if(globals.playChecker==1) {
-					for (var i = 0; i < count; i++) {
-						var item = project.activeLayer.children[i];
-							item.symbol.definition.style.fillColor = globals.noteColor;
-							item.position.x += item.bounds.width / 20;
-					if (item.bounds.left > view.size.width) {
-						item.position.x = -item.bounds.width;
-					}
-				}
-			}
-		}
+
+// The faster the user moves their mouse
+// the larger the circle will be
+// We dont want it to be larger than this
+tool.maxDistance = 50;
+
+
+// Returns an object specifying a semi-random color
+// The color will always have a red value of 0
+// and will be semi-transparent (the alpha value)
+function randomColor() {
+  
+  return {
+    red: 0,
+    green: Math.random(),
+    blue: Math.random(),
+    alpha: ( Math.random() * 0.25 ) + 0.05
+  };
+
+}
+
+
+// every time the user drags their mouse
+// this function will be executed
+function onMouseDrag(event) {
+
+  // Take the click/touch position as the centre of our circle
+  var x = event.middlePoint.x;
+  var y = event.middlePoint.y;
+  
+  // The faster the movement, the bigger the circle
+  var radius = event.delta.length / 2;
+  
+  // Generate our random color
+  var color = randomColor();
+
+  // Draw the circle 
+  drawCircle( x, y, radius, color );
+  
+   // Pass the data for this circle
+  // to a special function for later
+  emitCircle( x, y, radius, color );
+
+}
+
+
+function drawCircle( x, y, radius, color ) {
+
+  // Render the circle with Paper.js
+  var circle = new Path.Circle( new Point( x, y ), radius );
+  circle.fillColor = new RgbColor( color.red, color.green, color.blue, color.alpha );
+
+  // Refresh the view, so we always get an update, even if the tab is not in focus
+  view.draw();
+} 
+  
+
+// This function sends the data for a circle to the server
+// so that the server can broadcast it to every other user
+function emitCircle( x, y, radius, color ) {
+
+  // Each Socket.IO connection has a unique session id
+  var sessionId = io.socket.sessionid;
+  
+  // An object to describe the circle's draw data
+  var data = {
+    x: x,
+    y: y,
+    radius: radius,
+    color: color
+  };
+
+  // send a 'drawCircle' event with data and sessionId to the server
+  io.emit( 'drawCircle', data, sessionId )
+
+  // Lets have a look at the data we're sending
+  console.log( data )
+
+}
+
+
+// Listen for 'drawCircle' events
+// created by other users
+io.on( 'drawCircle', function( data ) {
+
+  console.log( 'drawCircle event recieved:', data );
+
+  // Draw the circle using the data sent
+  // from another user
+  drawCircle( data.x, data.y, data.radius, data.color );
+  
+})
