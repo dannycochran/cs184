f8732887507484920a292d3713d75d8f9a51059d
dannycochran <daniel@cochrans.org>
2013-03-21 17:57:25 -0700

    better audio loading2

--- public/javascripts/playNote.js
+++ public/javascripts/playNote.js
@@ -7,20 +7,26 @@ if ((navigator.userAgent.indexOf('Firefox') != -1) || (navigator.userAgent.index
 // initialize / load audio players
 var audioPlayerE = new Audio();
 audioPlayerE.src="strings/E" + extension
+audioPlayerE.preload = true;
 audioPlayerE.load();
 var audioPlayerA = new Audio();
 audioPlayerA.src="strings/A" + extension
+audioPlayerA.preload = true;
 audioPlayerA.load();
 var audioPlayerD = new Audio();
+audioPlayerD.preload = true;
 audioPlayerD.src="strings/D" + extension;
 audioPlayerD.load();
 var audioPlayerG = new Audio();
+audioPlayerG.preload = true;
 audioPlayerG.src="strings/G" + extension;
 audioPlayerG.load();
 var audioPlayerB = new Audio();
 audioPlayerB.src="strings/B" + extension;
+audioPlayerB.preload = true;
 audioPlayerB.load();
 var audioPlayerElittle = new Audio();
+audioPlayerElittle.preload = true;
 audioPlayerElittle.src="strings/Elittle" + extension;
 audioPlayerElittle.load();
 
