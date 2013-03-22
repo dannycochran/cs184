cc839f2a5c83328eafdb50fc9d103ae0c510c41c
dannycochran <daniel@cochrans.org>
2013-03-21 18:22:02 -0700

    another audio fix

--- public/javascripts/playNote.js
+++ public/javascripts/playNote.js
@@ -3,7 +3,6 @@ var extension = ".mp3"
 if ((navigator.userAgent.indexOf('Firefox') != -1) || (navigator.userAgent.indexOf('Opera') != -1)) {
     extension = ".ogg"
 }
-
 // initialize / load audio players
 var audioPlayerE = new Audio();
 audioPlayerE.src="strings/E" + extension
@@ -29,86 +28,89 @@ var audioPlayerElittle = new Audio();
 audioPlayerElittle.preload = true;
 audioPlayerElittle.src="strings/Elittle" + extension;
 audioPlayerElittle.load();
-
+    
+// if not using mobile, use stronger playback system, otherwise, use weak one
+if(!(navigator.userAgent.match(/iPhone/i) && navigator.userAgent.match(/iPad/i) && navigator.userAgent.match(/iPad/i) && navigator.userAgent.match(/Android/i))) {
+    console.log('hello')
+    // called in draw.js when rectangle clicked
+    playNote = function(index,count) {
+        if(index-count==0) {
+            audioPlayerA.pause();
+            audioPlayerD.pause();
+            audioPlayerG.pause();
+            audioPlayerB.pause();
+            audioPlayerElittle.pause();
+            audioPlayerE.load();
+            audioPlayerE.play();
+        }
+        else if(index-count==1) {
+            audioPlayerE.pause();
+            audioPlayerD.pause();
+            audioPlayerG.pause();
+            audioPlayerB.pause();
+            audioPlayerElittle.pause();
+            audioPlayerA.load();
+            audioPlayerA.play();
+        }
+        else if(index-count==2) {
+            audioPlayerE.pause();
+            audioPlayerA.pause();
+            audioPlayerG.pause();
+            audioPlayerB.pause();
+            audioPlayerElittle.pause();
+            audioPlayerD.load();
+            audioPlayerD.play();
+        }    
+        else if(index-count==3) {
+            audioPlayerE.pause();
+            audioPlayerA.pause();
+            audioPlayerD.pause();
+            audioPlayerB.pause();
+            audioPlayerElittle.pause();
+            audioPlayerG.load();
+            audioPlayerG.play();
+        }
+        else if(index-count==4) {
+            audioPlayerE.pause();
+            audioPlayerA.pause();
+            audioPlayerD.pause();
+            audioPlayerG.pause();
+            audioPlayerElittle.pause();
+            audioPlayerB.load();
+            audioPlayerB.play();
+        }
+        else if(index-count==5) {
+            audioPlayerE.pause();
+            audioPlayerA.pause();
+            audioPlayerD.pause();
+            audioPlayerG.pause();
+            audioPlayerB.pause();
+            audioPlayerElittle.load();
+            audioPlayerElittle.play();
+        }
+    }
+} else {
 // called in draw.js when rectangle clicked
-playNote = function(index,count) {
-    var noteStr;
-    if(index-count==0) {
-        audioPlayerA.pause();
-        audioPlayerA.load();
-        audioPlayerD.pause();
-        audioPlayerD.load();
-        audioPlayerG.pause();
-        audioPlayerG.load();
-        audioPlayerB.pause();
-        audioPlayerB.load();
-        audioPlayerElittle.pause();
-        audioPlayerElittle.load();
-        audioPlayerE.play();
-    }
-    else if(index-count==1) {
-        audioPlayerE.pause();
-        audioPlayerE.load();
-        audioPlayerD.pause();
-        audioPlayerD.load();
-        audioPlayerG.pause();
-        audioPlayerG.load();
-        audioPlayerB.pause();
-        audioPlayerB.load();
-        audioPlayerElittle.pause();
-        audioPlayerElittle.load();
-        audioPlayerA.play();
-    }
-    else if(index-count==2) {
-        audioPlayerE.pause();
-        audioPlayerE.load();
-        audioPlayerA.pause();
-        audioPlayerA.load();
-        audioPlayerG.pause();
-        audioPlayerG.load();
-        audioPlayerB.pause();
-        audioPlayerB.load();
-        audioPlayerElittle.pause();
-        audioPlayerElittle.load();
-        audioPlayerD.play();
-    }    
-    else if(index-count==3) {
-        audioPlayerE.pause();
-        audioPlayerE.load();
-        audioPlayerA.pause();
-        audioPlayerA.load();
-        audioPlayerD.pause();
-        audioPlayerD.load();
-        audioPlayerB.pause();
-        audioPlayerB.load();
-        audioPlayerElittle.pause();
-        audioPlayerElittle.load();
-        audioPlayerG.play();
-    }
-    else if(index-count==4) {
-        audioPlayerE.pause();
-        audioPlayerE.load();
-        audioPlayerA.pause();
-        audioPlayerA.load();
-        audioPlayerD.pause();
-        audioPlayerD.load();
-        audioPlayerG.pause();
-        audioPlayerG.load();
-        audioPlayerElittle.pause();
-        audioPlayerElittle.load();
-        audioPlayerB.play();
-    }
-    else if(index-count==5) {
-        audioPlayerE.pause();
-        audioPlayerE.load();
-        audioPlayerA.pause();
-        audioPlayerA.load();
-        audioPlayerD.pause();
-        audioPlayerD.load();
-        audioPlayerG.pause();
-        audioPlayerG.load();
-        audioPlayerB.pause();
-        audioPlayerB.load();
-        audioPlayerElittle.play();
+    playNote = function(index,count) {
+        if(index-count==0) {
+            audioPlayerE.play();
+        }
+        else if(index-count==1) {
+            audioPlayerA.play();
+        }
+        else if(index-count==2) {
+            audioPlayerD.play();
+        }    
+        else if(index-count==3) {
+            audioPlayerG.play();
+        }
+        else if(index-count==4) {
+            audioPlayerB.play();
+        }
+        else if(index-count==5) {
+            audioPlayerElittle.play();
+        }
     }
 }
+
+// if not using mobile, use stronger playback system, otherwise, use weak one
